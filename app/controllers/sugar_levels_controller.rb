4358f4199b0982787ad0f3ac0786b7d6c53399cc
StanisLove Sid <joingamings@gmail.com>
2018-09-10 16:17:17 +0300

    js create action sugar level

--- app/controllers/sugar_levels_controller.rb
+++ app/controllers/sugar_levels_controller.rb
@@ -78,11 +78,33 @@ class SugarLevelsController < ApplicationController
 
   def create
     @day = Day.find(params[:day_id])
+    @month = Month.find(@day.month_id)
+    @year = Year.find(@month.year_id)
     @day.sugar_levels.create(sugar_level_params)
     time_creation = "#{@day.created_at.year}"+"-"+
-      "#{@day.created_at.month}"+"-"+"#{@day.created_at.day} #{params[:sugar_level][:created_at]}"
+      "#{@day.created_at.month}"+"-"+"#{@day.created_at.day} #{params[:sugar_level][:created_at]}" 
     @day.sugar_levels.last.update(created_at: time_creation)
-    redirect_to :back
+    @sugar_level = @day.sugar_levels.last(2).first
+    @s_l = @day.sugar_levels.group_by_minute(:created_at).sum(:mmol)
+
+    @result = @s_l.without_emty_slots
+
+    @meals = @day.meals.group_by_minute(:created_at).sum(4)
+    @meals_result = @meals.without_emty_slots
+
+    @insulin = @day.insulin_injections.group_by_minute(:created_at).sum(3)
+    @insulin_result = @insulin.without_emty_slots
+
+    @exercise_start = @day.exercises.group_by_minute(:begining).sum(10)
+    @exercise_end = @day.exercises.group_by_minute(:ending).sum(10)
+
+    @warning_start = @day.warnings.group_by_minute(:begining).sum(15)
+    @warning_end = @day.warnings.group_by_minute(:ending).sum(15)
+    @prediction = @day.bsl_predictions.any? ? @day.bsl_predictions.last.prediction.round(2) : 0
+    respond_to do |format|
+      format.json { head :no_content }
+      format.js
+    end
   end
 
   private
